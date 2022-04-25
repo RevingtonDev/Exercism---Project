bd8d761f282391970e5ec39e94f35c42ba41c553
RevingtonDev <revington.dev@gmail.com>
2022-04-25 09:59:58 +0530

    Mobile Compatible short update.

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -34,69 +34,68 @@ export class Testimonial extends Component {
   render() {
     return (
       <div className="testimonial">
-        <div className="ts-d-lg">
-          <div className="lang-lg">
-            <img
-              src={this.props.track.track.icon_url}
-              alt="lang-logo"
-              className="lg"
-            />
+        <div className="ts-ud">
+          <div className="ts-d-lg">
+            <div className="lang-lg">
+              <img
+                src={this.props.track.track.icon_url}
+                alt="lang-logo"
+                className="lg ts-lang-ico"
+              />
+            </div>
+
+            <div className="mentor-lg">
+              <img
+                src={this.props.track.mentor.avatar_url}
+                alt="mentor-logo"
+                className="lg ts-mentor-ico"
+              />
+            </div>
           </div>
 
-          <div className="mentor-lg">
-            <img
-              src={this.props.track.mentor.avatar_url}
-              alt="mentor-logo"
-              className="lg"
-            />
-          </div>
-        </div>
-
-        <div className="ts-details">
-          <div className="ts-det">
-            <div className="ts-user">{this.props.track.mentor.handle}</div>
-            on{" "}
-            <span className="ts-exercise-title">
-              {this.props.track.exercise.title}
-            </span>{" "}
-            in{" "}
-            <span className="ts-user-track">
-              {this.props.track.track.title}
-            </span>
-            .
-          </div>
-          <div className="ts-exercise-info">
-            <img
-              className="ts-exercise-icon"
-              src={this.props.track.exercise.icon_url}
-              alt="Exercise"
-            />
+          <div className="ts-details">
+            <div className="ts-det">
+              <div className="ts-user">{this.props.track.mentor.handle}</div>
+              <div className="ts-exercise">
+                on{" "}
+                <span className="ts-exercise-title">
+                  {this.props.track.exercise.title}
+                </span>{" "}
+                in{" "}
+                <span className="ts-user-track">
+                  {this.props.track.track.title}
+                </span>
+                .
+              </div>
+            </div>
           </div>
         </div>
 
         <div className="ts-d-content">{this.props.track.content}</div>
 
-        <div className="ts-date">{this.updated}</div>
+        <div className="ts-last">
+          <div className="ts-date">{this.updated}</div>
 
-        <a href={this.props.link} className="ts-link">
-          <svg
-            version="1.1"
-            id="ts-capa"
-            width={24}
-            height={24}
-            viewBox="0 0 492.432 492.432"
-          >
-            <g id="XMLID_134_">
-              <path
-                id="ts-arrow"
-                strokeWidth={1.5}
-                d="M142.238,492.432c-9.79,0-19.588-3.736-27.05-11.209c-14.945-14.934-14.945-39.162,0-54.098l180.9-180.909
+          <a href={this.props.link} className="ts-link">
+            <svg
+              version="1.1"
+              id="ts-capa"
+              width={24}
+              height={24}
+              viewBox="0 0 492.432 492.432"
+            >
+              <g id="XMLID_134_">
+                <path
+                  id="ts-arrow"
+                  strokeWidth={1.5}
+                  d="M142.238,492.432c-9.79,0-19.588-3.736-27.05-11.209c-14.945-14.934-14.945-39.162,0-54.098l180.9-180.909
 		l-180.9-180.91c-14.945-14.935-14.945-39.163,0-54.098c14.926-14.944,39.172-14.944,54.098,0l207.96,207.958
 		c14.943,14.935,14.943,39.164,0,54.1l-207.96,207.957C161.824,488.697,152.026,492.432,142.238,492.432z"
-              />
-            </g>
-          </svg>
-        </a>
+                />
+              </g>
+            </svg>
+          </a>
+        </div>
       </div>
     );
   }
